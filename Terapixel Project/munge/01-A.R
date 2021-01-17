b61c32b1d87b2149622d9473b7e119fab730e09b
Peter Bevan <peterbevan@hotmail.co.uk>
2021-01-17 14:15:40 +0000

    TotalRender munging

--- Terapixel Project/munge/01-A.R
+++ Terapixel Project/munge/01-A.R
@@ -1,38 +1,12 @@
+library(ProjectTemplate)
+load.project()
 library(tidyverse)
 # Example preprocessing script.
 head(gpu)
 head(task.x.y)
-tail(application.checkpoints, 20)
 head(application.checkpoints)
-sum(application.checkpoints$jobId == "1024-lvl12-7e026be3-5fd0-48ee-b7d1-abd61f747705")
-application.checkpoints$timestamp[1] + application.checkpoints$timestamp[2]
-x <- difftime( as.POSIXct( application.checkpoints$timestamp[1], format="%Y-%m-%dT%H:%M"), 
-                                 as.POSIXct( application.checkpoints$timestamp[2], format="%Y-%m-%dT%H:%M:%OS"), 
-                                 units="min")
-x
-help("as.POSIXct")
-All_TotalRender = application.checkpoints[which(application.checkpoints$eventName=='TotalRender'),]
-All_TotalRender
-Sum_TotalRender = abs(difftime( as.POSIXct( All_TotalRender$timestamp[1], format="%Y-%m-%dT%H:%M"), 
-               as.POSIXct( All_TotalRender$timestamp[132080], format="%Y-%m-%dT%H:%M:%OS"), 
-               units="min"))
-library(glue)
-glue('The total time elapsed for TotalRender was {Sum_TotalRender} mins')
 
+TotalRender = filter(application.checkpoints, eventName=='TotalRender')
+TotalRender %>% mutate(timeN = as.POSIXct(timestamp, format = '%H:%M:%S')) %>% group_by(taskId) %>% summarise(timediff = difftime(first(timeN), last(timeN), unit = 'sec'))
 
-as.data.table(application.checkpoints)[, sum(cover), by = .(plotID, species)]
-
-application.checkpoints$timestamp <- as.POSIXct(strptime(levels(application.checkpoints$timestamp)[application.checkpoints$timestamp], format="%m-%d-%Y:%H.%M"))
-application.checkpoints %>% group_by(hostname, jobId, taskId, eventName)  %>% mutate(elapsed = difftime(timestamp, lag(timestamp))) %>% summarise(na.omit(elapsed))
-application.checkpoints
-
-
-as.data.table(application.checkpoints)[, sum(cover), by = .(plotID, species)]
-
-application.checkpoints$diffTime <- c(0, difftime(lubridate::ymd_hms(application.checkpoints$timestamp[-1]), 
-                                 lubridate::ymd_hms(application.checkpoints$timestamp[-nrow(application.checkpoints)]), units="auto"))
-diffTime <- application.checkpoints %>% group_by(hostname, jobId, taskId, eventName) %>% summarize((diffTime))
-
-##add to git
-
-sdfdhf
+TotalRender_elapsed = TotalRender %>% mutate(timeN = as.POSIXct( TotalRender$timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId) %>% summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec')))
