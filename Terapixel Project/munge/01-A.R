64ba4a782943c99a58bb365f4710a0a3eec8c026
Peter Bevan <peterbevan@hotmail.co.uk>
2021-01-19 14:11:06 +0000

    Making munging more concise

--- Terapixel Project/munge/01-A.R
+++ Terapixel Project/munge/01-A.R
@@ -6,48 +6,41 @@ head(gpu)
 head(task.x.y)
 head(application.checkpoints)
 
+library(tidyverse)
+#creating list of tasks for looping
+Tasks = c('TotalRender', 'Tiling', 'Saving Config', 'Render', 'Uploading')
+#looping through tasks in application checkpoints data and extracting runtimes
+for (i in Tasks){
+  nam = paste(i, 'Elapsed', sep = "_")
+  assign(nam, filter(application.checkpoints, eventName==i) %>% mutate(timeN = as.POSIXct(
+    timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId, hostname) %>%
+      summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec'))))
+}
+#creating list of data created above for joining
+Tasks_Elapsed = list(TotalRender_Elapsed, Tiling_Elapsed, `Saving Config_Elapsed`, Render_Elapsed, Uploading_Elapsed)
 
-##creating new dataframe for each eventName and their runtimes for each job, indexed by task id:
-#TotalRender
-TotalRender_elapsed = filter(application.checkpoints, eventName=='TotalRender') %>% mutate(timeN = as.POSIXct(
-  timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId, hostname) %>%
-  summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec')))
-#Tiling
-Tiling_elapsed = filter(application.checkpoints, eventName=='Tiling') %>% mutate(timeN = as.POSIXct(
-  timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId) %>%
-  summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec')))
-#Saving Config
-Saving_Config_elapsed = filter(application.checkpoints, eventName=='Saving Config') %>%
-  mutate(timeN = as.POSIXct(timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>%
-  group_by(taskId) %>% summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec')))
-#Render
-Render_elapsed = filter(application.checkpoints, eventName=='Render') %>% mutate(timeN = as.POSIXct(
-  timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId) %>% 
-  summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec')))
-#Uploading
-Uploading_elapsed = filter(application.checkpoints, eventName=='Uploading') %>% mutate(timeN = as.POSIXct(
-  timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId) %>%
-  summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec')))
-
-sum(TotalRender_elapsed$timediff)/60/60/24
-
-#Merging elapsed dataframes based on task id
-Runtimes = inner_join(TotalRender_elapsed, Tiling_elapsed, by='taskId')
-Runtimes = inner_join(Runtimes, Saving_Config_elapsed, by='taskId')
-Runtimes = inner_join(Runtimes, Render_elapsed, by='taskId')
-Runtimes = inner_join(Runtimes, Uploading_elapsed, by='taskId')
-#changing column names
+#joining data by task ID and hostname
+Runtimes = Tasks_Elapsed %>% reduce(left_join, by = c("taskId", "hostname"))
+#changing names of 'Runtimes' columns
 names(Runtimes) = c('taskId', 'hostname', 'TotalRender', 'Tiling', 'Saving_Config', 'Render', 'Uploading')
 
-
+#grouping gpus per hostname/vm and calculating means for gpu data
 gpu_performance = gpu %>% group_by(hostname, gpuSerial, gpuUUID) %>%
   summarise(mean_powerDrawWatt=mean(powerDrawWatt),
             mean_gpuTempC=mean(gpuTempC), mean_gpuUtilPerc=mean(gpuUtilPerc),
             mean_gpuMemUtilPerc=mean(gpuMemUtilPerc))
 
+#Grouping runtimes per hostname/vm and calculatin means for render data
 Runtimes_mean = Runtimes %>% group_by(hostname) %>%
   summarise(mean_TotalRender=mean(TotalRender), mean_Tiling=mean(Tiling),
             mean_SavingConfig=mean(Saving_Config), mean_Render=mean(Render),
             mean_Uploading=mean(Uploading))
 
+#merging gpu data and runtimes data by hostname
 gpu_performance = inner_join(gpu_performance, Runtimes_mean, by='hostname')
+
+summary(gpu_performance$mean_powerDrawWatt)
+
+plot(gpu_performance$mean_powerDrawWatt)
+
+
