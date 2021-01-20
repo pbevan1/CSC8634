63f13942cb28eb09de5e7aa783fc8f253c1cecf0
Peter Bevan <peterbevan@hotmail.co.uk>
2021-01-20 12:36:05 +0000

    Cache and worked on report

--- Terapixel Project/munge/01-A.R
+++ Terapixel Project/munge/01-A.R
@@ -1,26 +1,22 @@
-library(ProjectTemplate)
-load.project()
-library(tidyverse)
-# Example preprocessing script.
+
 head(gpu)
 head(task.x.y)
 head(application.checkpoints)
 
-library(tidyverse)
 #creating list of tasks for looping
 Tasks = c('TotalRender', 'Tiling', 'Saving Config', 'Render', 'Uploading')
 #looping through tasks in application checkpoints data and extracting runtimes
-for (i in Tasks){
-  nam = paste(i, 'Elapsed', sep = "_")
-  assign(nam, filter(application.checkpoints, eventName==i) %>% mutate(timeN = as.POSIXct(
-    timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId, hostname) %>%
-      summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec'))))
-}
-#creating list of data created above for joining
+#for (i in Tasks){
+#  nam = paste(i, 'Elapsed', sep = "_")
+#  assign(nam, filter(application.checkpoints, eventName==i) %>% mutate(timeN = as.POSIXct(
+#    timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId, hostname) %>%
+#      summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec'))))
+#}
+
 Tasks_Elapsed = list(TotalRender_Elapsed, Tiling_Elapsed, `Saving Config_Elapsed`, Render_Elapsed, Uploading_Elapsed)
 
 #joining data by task ID and hostname
-Runtimes = Tasks_Elapsed %>% reduce(left_join, by = c("taskId", "hostname"))
+Runtimes = Tasks_Elapsed %>% reduce(left_join, by = c("taskId", "hostname"), copy=TRUE)
 #changing names of 'Runtimes' columns
 names(Runtimes) = c('taskId', 'hostname', 'TotalRender', 'Tiling', 'Saving_Config', 'Render', 'Uploading')
 
@@ -38,9 +34,4 @@ Runtimes_mean = Runtimes %>% group_by(hostname) %>%
 
 #merging gpu data and runtimes data by hostname
 gpu_performance = inner_join(gpu_performance, Runtimes_mean, by='hostname')
-
-summary(gpu_performance$mean_powerDrawWatt)
-
-plot(gpu_performance$mean_powerDrawWatt)
-
 
--- Terapixel Project/src/eda.R
+++ Terapixel Project/src/eda.R
@@ -1,42 +1,43 @@
-library('ProjectTemplate')
-load.project()
-
 head(gpu_performance)
 
 (plt_power = ggplot(gpu_performance, aes(x=mean_powerDrawWatt)) + 
-  geom_histogram(color='white')) + ggtitle('Mean power draw of GPU (Watts)') +
+  geom_histogram(color='white')) + xlab('Mean power draw of GPU (Watts)') +
   theme(plot.title = element_text(hjust = 0.5))
 
 (plt_temp = ggplot(gpu_performance, aes(x=mean_gpuTempC)) + 
-    geom_histogram(color='white', binwidth=0.5)) + ggtitle('Mean temp of GPU (C)') +
+    geom_histogram(color='white', binwidth=0.5)) + xlab('Mean temp of GPU (C)') +
   theme(plot.title = element_text(hjust = 0.5))
 
 #Clearly multimodal (different classes of GPU??)
 (plt_utilPerc = ggplot(gpu_performance, aes(x=mean_gpuUtilPerc)) + 
-    geom_histogram(color='white')) + ggtitle('Mean utilisation of GPU Core(s) (%)') +
+    geom_histogram(color='white')) + xlab('Mean utilisation of GPU Core(s) (%)') +
   theme(plot.title = element_text(hjust = 0.5))
 
-(plt_memUtilPerc = ggplot(gpu_performance, aes(x=mean_gpuUtilPerc)) + 
-    geom_histogram(color='white')) + ggtitle('Mean utilisation of GPU memory (%)') +
+(plt_memUtilPerc = ggplot(gpu_performance, aes(x=mean_gpuMemUtilPerc)) + 
+    geom_histogram(color='white')) + xlab('Mean utilisation of GPU memory (%)') +
   theme(plot.title = element_text(hjust = 0.5))
 
 #multimodal as well - pull out lower group
 (plt_TotalRender = ggplot(gpu_performance, aes(x=mean_TotalRender)) + 
-    geom_histogram(color='red')) + ggtitle('Mean duration of total render task (s)') +
+    geom_histogram(color='red')) + xlab('Mean duration of total render task (s)') +
   theme(plot.title = element_text(hjust = 0.5))
 
 (plt_Tiling = ggplot(gpu_performance, aes(x=mean_Tiling)) + 
-    geom_histogram(color='red', binwidth=0.01)) + ggtitle('Mean duration of tiling (s)') +
+    geom_histogram(color='red', binwidth=0.01)) + xlab('Mean duration of tiling (s)') +
   theme(plot.title = element_text(hjust = 0.5))
 
 (plt_SavingConfig = ggplot(gpu_performance, aes(x=mean_SavingConfig)) + 
-    geom_histogram(color='red', binwidth=0.0001)) + ggtitle('Mean duration of configuration overhead (s)') +
+    geom_histogram(color='red', binwidth=0.0001)) + xlab('Mean duration of configuration overhead (s)') +
   theme(plot.title = element_text(hjust = 0.5))
 
 (plt_Render = ggplot(gpu_performance, aes(x=mean_Render)) + 
-    geom_histogram(color='red', binwidth=0.5)) + ggtitle('Mean duration of tiling (s)') +
+    geom_histogram(color='red', binwidth=0.5)) + xlab('Mean duration of tiling (s)') +
   theme(plot.title = element_text(hjust = 0.5))
 
 (plt_Uploading = ggplot(gpu_performance, aes(x=mean_Uploading)) + 
-    geom_histogram(color='red', binwidth=0.03)) + ggtitle('Mean duration of tiling (s)') +
+    geom_histogram(color='red', binwidth=0.03)) + xlab('Mean duration of tiling (s)') +
   theme(plot.title = element_text(hjust = 0.5))
+
+gpu_grid = grid.arrange(plt_temp, plt_utilPerc, plt_memUtilPerc, nrow = 2)
+
+render_grid = grid.arrange(plt_TotalRender, plt_Tiling, plt_SavingConfig, plt_Render, plt_Uploading, nrow = 3)
