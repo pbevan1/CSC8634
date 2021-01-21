4576e9f8f87606cb87e61a2b29cec8ff09e25f96
Peter Bevan <peterbevan@hotmail.co.uk>
2021-01-21 19:12:16 +0000

    more eda and report writing

--- Terapixel Project/munge/01-A.R
+++ Terapixel Project/munge/01-A.R
@@ -20,18 +20,29 @@ Runtimes = Tasks_Elapsed %>% reduce(left_join, by = c("taskId", "hostname"), cop
 #changing names of 'Runtimes' columns
 names(Runtimes) = c('taskId', 'hostname', 'TotalRender', 'Tiling', 'Saving_Config', 'Render', 'Uploading')
 
+gm_mean = function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+
 #grouping gpus per hostname/vm and calculating means for gpu data
+#geometric mean used for ratios.
+#Converted to numeric for analysis
 gpu_performance = gpu %>% group_by(hostname, gpuSerial, gpuUUID) %>%
-  summarise(mean_powerDrawWatt=mean(powerDrawWatt),
-            mean_gpuTempC=mean(gpuTempC), mean_gpuUtilPerc=mean(gpuUtilPerc),
-            mean_gpuMemUtilPerc=mean(gpuMemUtilPerc))
+  summarise(mean_powerDrawWatt=as.numeric(mean(powerDrawWatt)),
+            mean_gpuTempC=mean(gpuTempC), mean_gpuUtilPerc=as.numeric(gm_mean(gpuUtilPerc)),
+            mean_gpuMemUtilPerc=as.numeric(gm_mean(gpuMemUtilPerc)))
 
 #Grouping runtimes per hostname/vm and calculatin means for render data
+#Converted to numeric for analysis
 Runtimes_mean = Runtimes %>% group_by(hostname) %>%
-  summarise(mean_TotalRender=mean(TotalRender), mean_Tiling=mean(Tiling),
-            mean_SavingConfig=mean(Saving_Config), mean_Render=mean(Render),
-            mean_Uploading=mean(Uploading))
+  summarise(mean_TotalRender=as.numeric(mean(TotalRender)), mean_Tiling=as.numeric(mean(Tiling)),
+            mean_SavingConfig=as.numeric(mean(Saving_Config)), mean_Render=as.numeric(mean(Render)),
+            mean_Uploading=as.numeric(mean(Uploading)))
 
 #merging gpu data and runtimes data by hostname
 gpu_performance = inner_join(gpu_performance, Runtimes_mean, by='hostname')
 
+#removing hostname and gpu id as not providing additional information
+gpu_performance = subset(gpu_performance, select = -c(1, 3))
+#converting gpu serial number to rowname to allow PCA but still index by gpu
+gpu_performance = data.frame(column_to_rownames(gpu_performance, var = "gpuSerial"))
--- Terapixel Project/src/eda.R
+++ Terapixel Project/src/eda.R
@@ -1,43 +1,18 @@
 head(gpu_performance)
 
-(plt_power = ggplot(gpu_performance, aes(x=mean_powerDrawWatt)) + 
-  geom_histogram(color='white')) + xlab('Mean power draw of GPU (Watts)') +
-  theme(plot.title = element_text(hjust = 0.5))
 
-(plt_temp = ggplot(gpu_performance, aes(x=mean_gpuTempC)) + 
-    geom_histogram(color='white', binwidth=0.5)) + xlab('Mean temp of GPU (C)') +
-  theme(plot.title = element_text(hjust = 0.5))
-
-#Clearly multimodal (different classes of GPU??)
-(plt_utilPerc = ggplot(gpu_performance, aes(x=mean_gpuUtilPerc)) + 
-    geom_histogram(color='white')) + xlab('Mean utilisation of GPU Core(s) (%)') +
-  theme(plot.title = element_text(hjust = 0.5))
-
-(plt_memUtilPerc = ggplot(gpu_performance, aes(x=mean_gpuMemUtilPerc)) + 
-    geom_histogram(color='white')) + xlab('Mean utilisation of GPU memory (%)') +
-  theme(plot.title = element_text(hjust = 0.5))
-
-#multimodal as well - pull out lower group
-(plt_TotalRender = ggplot(gpu_performance, aes(x=mean_TotalRender)) + 
-    geom_histogram(color='red')) + xlab('Mean duration of total render task (s)') +
-  theme(plot.title = element_text(hjust = 0.5))
-
-(plt_Tiling = ggplot(gpu_performance, aes(x=mean_Tiling)) + 
-    geom_histogram(color='red', binwidth=0.01)) + xlab('Mean duration of tiling (s)') +
-  theme(plot.title = element_text(hjust = 0.5))
-
-(plt_SavingConfig = ggplot(gpu_performance, aes(x=mean_SavingConfig)) + 
-    geom_histogram(color='red', binwidth=0.0001)) + xlab('Mean duration of configuration overhead (s)') +
-  theme(plot.title = element_text(hjust = 0.5))
-
-(plt_Render = ggplot(gpu_performance, aes(x=mean_Render)) + 
-    geom_histogram(color='red', binwidth=0.5)) + xlab('Mean duration of tiling (s)') +
-  theme(plot.title = element_text(hjust = 0.5))
-
-(plt_Uploading = ggplot(gpu_performance, aes(x=mean_Uploading)) + 
-    geom_histogram(color='red', binwidth=0.03)) + xlab('Mean duration of tiling (s)') +
-  theme(plot.title = element_text(hjust = 0.5))
 
 gpu_grid = grid.arrange(plt_temp, plt_utilPerc, plt_memUtilPerc, nrow = 2)
 
 render_grid = grid.arrange(plt_TotalRender, plt_Tiling, plt_SavingConfig, plt_Render, plt_Uploading, nrow = 3)
+
+ggplot(gpu_performance, aes(x=mean_SavingConfig)) + 
+  geom_histogram(color='red', binwidth=0.0001) + xlab('Mean duration of configuration overhead (s)') +
+  theme(plot.title = element_text(hjust = 0.5)) + theme(text = element_text(size=8))
+
+dim(gpu)
+
+
+
+
+pca_gpu_perf = prcomp(gpu_performance, scale=TRUE)
