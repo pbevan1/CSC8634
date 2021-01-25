ad03e233853f1de238f79031bfdeac75bb5a9e0c
Peter Bevan <peterbevan@hotmail.co.uk>
2021-01-25 10:45:21 +0000

    Modelling and reports

--- Terapixel Project/munge/01-A.R
+++ Terapixel Project/munge/01-A.R
@@ -1,30 +1,40 @@
 
-head(gpu)
-head(task.x.y)
-head(application.checkpoints)
+#creating list of event names for looping
+Events = c('TotalRender', 'Tiling', 'Saving Config', 'Render', 'Uploading')
+##cached## looping through event names in application checkpoints data and extracting runtimes
+##cached## for (i in Events){
+##cached##   nam = paste(i, 'Elapsed', sep = "_")
+##cached##   assign(nam, filter(application.checkpoints, eventName==i) %>% mutate(timeN = as.POSIXct(
+##cached##     timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId, hostname) %>%
+##cached##       summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec'))))
+##cached## }
 
-#creating list of tasks for looping
-Tasks = c('TotalRender', 'Tiling', 'Saving Config', 'Render', 'Uploading')
-#looping through tasks in application checkpoints data and extracting runtimes
-#for (i in Tasks){
-#  nam = paste(i, 'Elapsed', sep = "_")
-#  assign(nam, filter(application.checkpoints, eventName==i) %>% mutate(timeN = as.POSIXct(
-#    timestamp, format="%Y-%m-%dT%H:%M:%OS"))%>% group_by(taskId, hostname) %>%
-#      summarise(timediff = abs(difftime(first(timeN), last(timeN), unit = 'sec'))))
-#}
+Events_Elapsed = list(TotalRender_Elapsed, Tiling_Elapsed, `Saving Config_Elapsed`, Render_Elapsed, Uploading_Elapsed)
 
-Tasks_Elapsed = list(TotalRender_Elapsed, Tiling_Elapsed, `Saving Config_Elapsed`, Render_Elapsed, Uploading_Elapsed)
+##cached## #joining data by task ID and hostname
+##cached## Runtimes = Events_Elapsed %>% reduce(left_join, by = c("taskId", "hostname"), copy=TRUE)
+##cached## #joining task.x.y on
+##cached## Runtimes = Runtimes %>% left_join(task.x.y[,-2], by="taskId")
+##cached## #changing names of 'Runtimes' columns
+##cached## names(Runtimes) = c('taskId', 'hostname', 'TotalRender', 'Tiling',
+##cached##                     'Saving_Config', 'Render', 'Uploading', 'x', 'y', 'Level')
+##cached## #after noticing TotalRender does not include Tiling, adding new column including tiling.
+##cached## Runtimes = Runtimes %>% mutate(TotalRender_IncTil=Uploading+Saving_Config+Render+Tiling)
+##cached## #Reordering Columns to be more presentable
+##cached## Runtimes = Runtimes[,c(1,2,11,3:10)]
+##cached## Runtimes[, 3:8] <- sapply(Runtimes[, 3:8], as.numeric)
 
-#joining data by task ID and hostname
-Runtimes = Tasks_Elapsed %>% reduce(left_join, by = c("taskId", "hostname"), copy=TRUE)
-#changing names of 'Runtimes' columns
-names(Runtimes) = c('taskId', 'hostname', 'TotalRender', 'Tiling', 'Saving_Config', 'Render', 'Uploading')
+#filtering different levels and saving as different dataframes
+Runtimes_12 = filter(Runtimes, Level==12)
+Runtimes_8 = filter(Runtimes, Level==8)
+Runtimes_4 = filter(Runtimes, Level==4)
 
+#defining function for geometric mean to summarise ratio values
 gm_mean = function(x, na.rm=TRUE){
   exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
 }
 
-#grouping gpus per hostname/vm and calculating means for gpu data
+#grouping gpus by gpu serial number and calculating means for gpu data
 #geometric mean used for ratios.
 #Converted to numeric for analysis
 gpu_performance = gpu %>% group_by(hostname, gpuSerial, gpuUUID) %>%
@@ -32,17 +42,41 @@ gpu_performance = gpu %>% group_by(hostname, gpuSerial, gpuUUID) %>%
             mean_gpuTempC=mean(gpuTempC), mean_gpuUtilPerc=as.numeric(gm_mean(gpuUtilPerc)),
             mean_gpuMemUtilPerc=as.numeric(gm_mean(gpuMemUtilPerc)))
 
-#Grouping runtimes per hostname/vm and calculatin means for render data
+#Grouping runtimes per hostname/vm and calculating means for render data
 #Converted to numeric for analysis
-Runtimes_mean = Runtimes %>% group_by(hostname) %>%
-  summarise(mean_TotalRender=as.numeric(mean(TotalRender)), mean_Tiling=as.numeric(mean(Tiling)),
+Runtimes_12_mean = Runtimes_12 %>% group_by(hostname) %>%
+  summarise(mean_TotalRender_IncTil=as.numeric(mean(TotalRender_IncTil)), mean_TotalRender=as.numeric(mean(TotalRender)), mean_Tiling=as.numeric(mean(Tiling)),
             mean_SavingConfig=as.numeric(mean(Saving_Config)), mean_Render=as.numeric(mean(Render)),
-            mean_Uploading=as.numeric(mean(Uploading)))
+            mean_Uploading=as.numeric(mean(Uploading)), mean_Level=as.numeric(mean(Level)))
+Runtimes_8_mean = Runtimes_8 %>% group_by(hostname) %>%
+  summarise(mean_TotalRender_IncTil=as.numeric(mean(TotalRender_IncTil)), mean_TotalRender=as.numeric(mean(TotalRender)), mean_Tiling=as.numeric(mean(Tiling)),
+            mean_SavingConfig=as.numeric(mean(Saving_Config)), mean_Render=as.numeric(mean(Render)),
+            mean_Uploading=as.numeric(mean(Uploading)), mean_Level=as.numeric(mean(Level)))
+Runtimes_4_mean = Runtimes_4 %>% group_by(hostname) %>%
+  summarise(mean_TotalRender_IncTil=as.numeric(mean(TotalRender_IncTil)), mean_TotalRender=as.numeric(mean(TotalRender)), mean_Tiling=as.numeric(mean(Tiling)),
+            mean_SavingConfig=as.numeric(mean(Saving_Config)), mean_Render=as.numeric(mean(Render)),
+            mean_Uploading=as.numeric(mean(Uploading)), mean_Level=as.numeric(mean(Level)))
 
-#merging gpu data and runtimes data by hostname
-gpu_performance = inner_join(gpu_performance, Runtimes_mean, by='hostname')
+#merging gpu data and runtimes data by hostname (one entry per gpu)
+gpu_performance_12 = inner_join(gpu_performance, Runtimes_12_mean, by='hostname')
+gpu_performance_8 = inner_join(gpu_performance, Runtimes_8_mean, by='hostname')
+gpu_performance_4 = inner_join(gpu_performance, Runtimes_4_mean, by='hostname')
 
 #removing hostname and gpu id as not providing additional information
-gpu_performance = subset(gpu_performance, select = -c(1, 3))
+gpu_performance_12 = subset(gpu_performance_12, select = -c(1, 3, 14))
+gpu_performance_8 = subset(gpu_performance_8, select = -c(1, 3, 14))
+gpu_performance_4 = subset(gpu_performance_4, select = -c(1, 3, 14))
+
 #converting gpu serial number to rowname to allow PCA but still index by gpu
-gpu_performance = data.frame(column_to_rownames(gpu_performance, var = "gpuSerial"))
+gpu_performance_12 = data.frame(column_to_rownames(gpu_performance_12, var = "gpuSerial"))
+gpu_performance_8 = data.frame(column_to_rownames(gpu_performance_8, var = "gpuSerial"))
+gpu_performance_4 = data.frame(column_to_rownames(gpu_performance_4, var = "gpuSerial"))
+
+#computing sum of all event type runtimes (seconds)
+Runtime_Totals = as.data.frame(colSums(Runtimes[,c(5:8)]))
+colnames(Runtime_Totals) = 'Totals'
+Runtime_Totals = tibble::rownames_to_column(Runtime_Totals, "Event")
+Runtime_Totals = Runtime_Totals %>% mutate(perc=percent(Totals/sum(Totals)))
+Runtime_Totals$Event = c('Tiling (2.23%)', 'Saving_Config (0.01%)', 'Render (94.56%)', 'Uploading (3.20%)')
+
+cbbPalette = c('#000000', "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
--- Terapixel Project/src/eda.R
+++ Terapixel Project/src/eda.R
@@ -1,6 +1,7 @@
 head(gpu_performance)
 
 
+pairs(gpu_performance, col = rgb(red = 0, green = 0, blue = 0, alpha = 0.1))
 
 gpu_grid = grid.arrange(plt_temp, plt_utilPerc, plt_memUtilPerc, nrow = 2)
 
@@ -15,4 +16,61 @@ dim(gpu)
 
 
 
-pca_gpu_perf = prcomp(gpu_performance, scale=TRUE)
+
+plot(pca_gpu_perf, type="l")
+
+(pc1_pc2_gpu = pca_gpu_perf$rotation[,c(1:2)])
+
+pca_gpu_perf$rotation[,c(1:2)]
+
+
+
+#kmeans function with K=2 as identified by above plot
+km_2 = kmeans(gpu_performance_12[,-6], 2, iter.max=50, nstart=20)
+#kmeans function with K=2 as identified by above plot
+km_4 = kmeans(gpu_performance_12, 4, iter.max=50, nstart=20)
+
+#examining the structure of the object
+str(km_2)
+km_2$cluster
+
+## Plot the first PC against the second PC using the cluster allocation to set
+## different colours (col) and plotting characters (pch) for each cluster:
+plot(pca_gpu_perf$x[,1], pca_gpu_perf$x[,2], main="Cluster solutions in ISLR data in space of first two PCs", xlab="First PC", ylab="Second PC",
+     col=km_2$cluster, pch=km_2$cluster)
+plot(pca_gpu_perf$x[,1], pca_gpu_perf$x[,2], main="Cluster solutions in ISLR data in space of first two PCs", xlab="First PC", ylab="Second PC",
+     col=km_4$cluster, pch=km_4$cluster)
+## Add labels representing the patient numbers:
+#text(pca_gpu_perf$x[,1], pca_gpu_perf$x[,2], labels=rownames(gpu_performance), cex=0.5, pos=3,
+#col="darkgrey")
+
+#kmeans function with K=2 as identified by above plot
+km_4 = kmeans(gpu_performance_12, 4, iter.max=50, nstart=20)
+#examining the structure of the object
+str(km_4)
+
+hostname_sample_1 = Runtimes_12[Runtimes_12$hostname==sample(Runtimes_12$hostname,1),]
+hostname_sample_2 = Runtimes_12[Runtimes_12$hostname==sample(Runtimes_12$hostname,1),]
+hostname_sample_3 = Runtimes_12[Runtimes_12$hostname==sample(Runtimes_12$hostname,1),]
+
+#plotting heatmap of runtimes with respect to the coordinates of the tiles
+heatmap = ggplot(Runtimes_12, aes(x, y, fill= TotalRender_IncTil)) +
+  geom_tile() +
+  scale_fill_gradient(low="white", high="red",
+                      limits=range(Runtimes_12$TotalRender_IncTil))
+tmp <- ggplot_build(heatmap)$data[[1]][2,]  # get the first data point from geom_raster
+width <- tmp$xmax - tmp$xmin  # calculate the width of the rectangle
+height <- tmp$ymax - tmp$ymin  # calculate the height of the rectangle
+heatmap + geom_tile(data = hostname_sample_1,  width = width, height = height, fill = "blue") + 
+  geom_tile(data = hostname_sample_2,  width = width, height = height, fill = "green") + 
+  geom_tile(data = hostname_sample_3,  width = width, height = height, fill = "yellow")
+
+#Creating scatterplot matrix with k-means clusters used to colour groups
+#clustered on the full dataset.
+pairs(gpu_performance_12[,-6], col = alpha(km_4$cluster, 0.1))
+
+
+## Plot the first PC against the second PC using the cluster allocation to set
+## different colours (col) and plotting characters (pch) for each cluster:
+plot(pca_gpu_perf$x[,1], pca_gpu_perf$x[,2], main="Cluster solutions in ISLR data in space of first two PCs", xlab="First PC", ylab="Second PC",
+     col=km_4$cluster, pch=km_4$cluster)
